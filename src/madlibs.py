c02005df32e5f5febde95b332ef7d83bc03ae1b0
Filipe Andrade <filipeyay@proton.me>
2025-11-14 12:37:29 -0300

    added madlibs project source code

---
+++ src/madlibs.py
@@ -0,0 +1,12 @@
+# madlibs game
+
+adjective1 = input("Enter a adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter a adjective (description): ")
+verb1 = input("Enter a verb endeing with 'ing': ")
+adjective3 = input("Enter a adjective (description): ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}.")
+print(f"I was {adjective3}!")
